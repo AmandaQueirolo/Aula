75897bc25f2f1ae5a7235df7338d36a7705441ce
AmandaQueirolo <mandi.queirolo@gmail.com>
2016-11-03 10:46:57 -0200

    Update login.php

--- login.php
+++ login.php
@@ -13,7 +13,7 @@
 					<input type="text" name="email1" value="">
 					<p>Senha</p>
 					<input type="password" name="senha1" value="">
-						<input type="submit" value="Continuar">
+					<input type="submit" value="Continuar">
 				</fieldset>
 			</center>
 		</form>
