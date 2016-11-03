93515ecf1e5fb9ce39c690fa9620820b206bb552
AmandaQueirolo <mandi.queirolo@gmail.com>
2016-11-03 10:43:35 -0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang = "pt-br" charset = "utf-8">
+	<head>
+		<title>Agenda</title>
+		<link rel="stylesheet" type="text/css" href="agenda.css">
+	</head>
+	<body>
+		<form action="listar2.php" method="POST">
+			<center>
+				<fieldset>
+					<legend>Tela de Login</legend>
+					<p>Login</p>
+					<input type="text" name="email1" value="">
+					<p>Senha</p>
+					<input type="password" name="senha1" value="">
+						<input type="submit" value="Continuar">
+				</fieldset>
+			</center>
+		</form>
+</body>
